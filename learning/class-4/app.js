87605c6f80c73c1f4c8b79fbec3bda392cc82a47
BrandonArgel <brandargel@gmail.com>
2026-03-20 08:49:01 -0600

    Fix CORS middleware usage and default port

--- learning/class-4/app.js
+++ learning/class-4/app.js
@@ -4,12 +4,12 @@ import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 app.use(json())
-app.use(corsMiddleware)
+app.use(corsMiddleware())
 app.disable('x-powered-by')
 
 app.use('/movies', moviesRouter)
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 5000
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
